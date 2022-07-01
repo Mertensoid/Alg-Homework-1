69d79084549276df2b9037834ac65a2c4174e6d6
Mertensoid <matsoukm@yandex.ru>
2022-07-01 18:41:26 +0300

    Initial Commit

---
+++ Alg-Homework-1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Alg-Homework-1
+//
+//  Created by admin on 01.07.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
